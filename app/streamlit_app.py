e1058e59e76bcb6f8d8fdd326809f4a8ebb886f5
Nier Finik <139979165+Voerdo@users.noreply.github.com>
2025-11-28 05:19:21 +0300

    Add files via upload

---
+++ app/streamlit_app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from sidebar2 import display_sidebar
+from chat_interface import display_chat_interface
+
+st.title("RAG. База знаний компании.")
+
+# Initialize session state variables
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "session_id" not in st.session_state:
+    st.session_state.session_id = None
+    
+if "model" not in st.session_state:
+    st.session_state.model = "llama3.2"
+
+# Display the sidebar
+with st.sidebar:
+    display_sidebar()
+
+# Display the chat interface
+display_chat_interface()
